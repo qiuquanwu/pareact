9c082490201c7587cae711ed95a6e08750ee975e
qiuquanwu <919403255@qq.com>
2019-07-31 21:43:53 +0800

    add react-router- dom suport

---
+++ src/components/about/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <h3>About</h3>
+         );
+    }
+}
+ 
+export default About;
---
+++ src/components/home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <h3>Home</h3>
+         );
+    }
+}
+ 
+export default Home;
