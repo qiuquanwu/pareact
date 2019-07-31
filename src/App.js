cc5ecc54999c961dfec2db314b94ff81cdb5cb7d
qiuquanwu <919403255@qq.com>
2019-07-31 20:54:31 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+function App() {
+    return(
+        <div id='contain'>
+            <h1>Wellcom to Pareact</h1>
+        </div>
+    );
+}
+export default App;
